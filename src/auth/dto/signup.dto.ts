5a4f4d14900bd564b6f037bfb64feff0c0dbd7a4
민지영 <kuck523@gmail.com>
2025-11-03 17:08:54 +0900

    refactor: user role default 수정

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -6,11 +6,7 @@ const phoneRegex = /^01[0-9]-?[0-9]{3,4}-?[0-9]{4}$/;
 const emailRegex = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 export const SignupSchema = z.object({
-  role: z
-    .enum(Role, {
-      message: '올바른 역할을 선택해주세요.',
-    })
-    .default(Role.CUSTOMER),
+  role: z.enum(Role).optional().default(Role.RETAILER),
   email: z.string().regex(emailRegex, '올바른 이메일 형식이 아닙니다.'),
   password: z.string().min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
   name: z.string().min(1, '이름을 입력해주세요.'),
