1926df773de02f4d797e3b5ddec69d0c21fd2bd3
민지영 <kuck523@gmail.com>
2025-11-05 02:14:36 +0900

    refactor: User 모듈 구조 변경에 따른 연동 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserModule } from '@/user/user.module';
+import { UserModule } from '@/user/infrastructure/user.module';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
