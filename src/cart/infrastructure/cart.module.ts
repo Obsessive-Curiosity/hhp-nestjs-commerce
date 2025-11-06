c854ac005a66b1d39afdfc6b70705714be1febca
민지영 <kuck523@gmail.com>
2025-11-06 21:32:28 +0900

    feat: cart

---
+++ src/cart/infrastructure/cart.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { RedisModule } from '../../redis/redis.module';
+import { ProductModule } from '../../product/infrastructure/product.module';
+import { CartService } from '../domain/service/cart.service';
+import { CART_REPOSITORY } from '../domain/interface/cart.repository.interface';
+import { CartRepository } from './cart.repository';
+import { CartFacade } from '../application/cart.facade';
+import { CartController } from '../presentation/controller/cart.controller';
+import { CartUseCase } from '../application/cart.usecase';
+
+@Module({
+  imports: [RedisModule, ProductModule],
+  controllers: [CartController],
+  providers: [
+    CartService,
+    CartFacade,
+    CartUseCase,
+    {
+      provide: CART_REPOSITORY,
+      useClass: CartRepository,
+    },
+  ],
+  exports: [CartService, CartFacade],
+})
+export class CartModule {}
